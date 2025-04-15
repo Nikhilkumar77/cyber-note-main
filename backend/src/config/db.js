fb104ebe4ce3ed6a82228475ab344ea0afa99d98
Nikhilkumar77 <nikhilkumar.pr6@gmail.com>
2025-04-15 18:18:55 +0530

    backend surver

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const letsConnect = await mongoose.connect(process.env.MONGO_URI, {
+    const letsConnect = await mongoose.connect("mongodb+srv://nikhil:nikhil@cluster0.1urrif2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
       useNewUrlParser: true,
     });
     console.log(`MongoDB is connected`);
